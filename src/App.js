62e181f90f36d85b51d46c4276d2d111f3dd130d
aimyv <av533@bath.ac.uk>
2022-11-14 12:54:39 +0000

    now resolved

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { CreateGame, Game, JoinGame, Leaderboard, Lobby, Menu, NotFound, Results } from './pages';
+import { CreateGame, Game, JoinGame, Leaderboard, Lobby, Menu, NotFound, Results } from './Pages';
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* <CreateGame />
-      <Game />
-      <JoinGame />
-      <Leaderboard />
-      <Lobby /> */}
-      <Menu />
-      {/* <Results /> */}
-=======
       <Routes>
         <Route path="/" element={<Menu />} />
         <Route path="/create" element={<CreateGame />} />
@@ -23,8 +14,6 @@ function App() {
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <CreateGame />
->>>>>>> a3d40e72cf06ee74e9291d2639673912dba83ce2
     </div>
   );
 }
