a70d3ffcec085269900388f76eec216d895feb3e
aimyv <av533@bath.ac.uk>
2022-11-14 14:48:31 +0000

    added or fieldset on menu

--- src/Pages/Menu/index.js
+++ src/Pages/Menu/index.js
@@ -6,6 +6,11 @@ export default function Menu() {
             <h1>Game</h1>
             <div className='center'>
                 <button id='create'>Create Game</button>
+
+                <fieldset>
+                    <legend>or</legend>
+                </fieldset>
+
                 <button id='join'>Join Game</button>
             </div>
             <button id='leaderboard'>Leaderboard</button>
