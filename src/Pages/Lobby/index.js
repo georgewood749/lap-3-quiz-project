3f0f4d4a3accd55e70c8df8b424fcd7bfcd75950
aimyv <av533@bath.ac.uk>
2022-11-15 12:31:16 +0000

    styled lobby

--- src/Pages/Lobby/index.js
+++ src/Pages/Lobby/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import loading from "./loading.png";
 
 export default function Lobby() {
+
+    const currentMeeting = 'xxx'
+    const players = 0 // to change
+
     return (
         <div>
             <h1>Lobby</h1>
-            <img className="centering" src={loading} alt="Loading" />
+            <div id='lobbyID'>Meeting ID: {currentMeeting}</div>
+            <div id='lobby' className='center'>
+                <img src={loading} alt="Loading" />
+                <h2 id='playerCount'>{players} players in lobby...</h2>
+            </div>
         </div>
     )
 }
