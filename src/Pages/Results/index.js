5952bd5eb84e91b06e829024f54f78ef4b062f02
aimyv <av533@bath.ac.uk>
2022-11-15 17:55:29 +0000

    added results page

--- src/Pages/Results/index.js
+++ src/Pages/Results/index.js
@@ -2,6 +2,29 @@ import React from 'react'
 
 export default function Results() {
     return (
-        <div>Results</div>
+        <div>
+            <h1>
+                Podium
+            </h1>
+            <div id='winner'>
+                Congratulations first!
+            </div>
+            <div className='leaderboardRow'>
+                <h2>first 🥇</h2>
+                <h2>1000</h2>
+            </div>
+            <div className='leaderboardRow'>
+                <h2>second 🥈</h2>
+                <h2>750</h2>
+            </div>
+            <div className='leaderboardRow'>
+                <h2>third 🥉</h2>
+                <h2>500</h2>
+            </div>
+            <div className='leaderboardRow'>
+                <h2>fourth</h2>
+                <h2>250</h2>
+            </div>
+        </div>
     )
 }
