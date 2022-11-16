a5b84d80293d9fbead10f034ba7e61ddc271c002
aimyv <av533@bath.ac.uk>
2022-11-16 13:10:10 +0000

    tests passing with electron browser

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -45,19 +45,19 @@ describe("Menu", () => {
 
   it('allows user to navigate from menu to create game form', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('#create').click({force: true});
+    cy.get('#create').click();
     cy.location('pathname').should('match', /\/create$/);
   })
 
   it('allows user to navigate from menu to join game form', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('#join').click({force: true});
+    cy.get('#join').click();
     cy.location('pathname').should('match', /\/join$/);
   })
 
   it('allows user to navigate from menu to leaderboard', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('#leaderboard').click({force: true});
+    cy.get('#leaderboard').click();
     cy.location('pathname').should('match', /\/leaderboard$/);
   })
 
@@ -70,12 +70,27 @@ describe("Create Game", () => {
 
   it('allows user to fill in the create game form and submit it', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('#create').click({force: true});
-    cy.get('#difficulty', {withinSubject:null}).select('easy', { force: true });
-    cy.get('#numQuestions').clear().type('10', { force: true });
-    cy.get('#category', {withinSubject:null}).select('9', { force: true });
-    cy.get('#questionType', {withinSubject:null}).select('multiple', { force: true });
-    cy.get('[type="submit"]').click({ force: true });
+    cy.get('#create').click();
+    cy.get('#difficulty').select('easy');
+    cy.get('#numQuestions').type('10');
+    cy.get('#category').select('9');
+    cy.get('#questionType').select('multiple');
+    cy.get('[type="submit"]').click();
+  })
+
+})
+
+describe("Join Game", () => {
+  beforeEach(() => {
+    cy.viewport(1600, 900);
+  })
+
+  it('allows user to fill in the join game form and submit it', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('#join').click();
+    cy.get('#join > :nth-child(1) > input').clear().type('123456');
+    cy.get('#join > :nth-child(2) > input').clear().type('testUsername');
+    cy.get('[type="submit"]').click();
   })
 
 })
