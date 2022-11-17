1973b1f488297ffe11f085b2e997c661ba75d88a
kaiyuleung <kyl1g16.ecs@gmail.com>
2022-11-17 13:03:44 +0000

    results left to do

--- src/Pages/Game/index.js
+++ src/Pages/Game/index.js
@@ -1,20 +1,55 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { store_answers } from '../../actions/socket/socketSlice';
 
 
 export default function Game() {
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     const qa = useSelector(state => state.socket.qa);
     const username = useSelector(state => state.socket.user.username);
+    const scores = useSelector(state => state.socket.user.scores);
+    const socket = useSelector(state => state.socket.socket);
 
-    const [ content, setContent] = useState({ question: "Question", answer: [ "" ] })
+    const [ content, setContent] = useState({ question: "Question", answers: [ "" ] })
+    const [ answer, setAnswer ] = useState("")
+    const [ nQsAnsed, setNQsAnsed] = useState(0)
+
+    function mixAns(incorrect_answers, correct_answer){
+        let ranIdx = Math.floor(Math.random() * (incorrect_answers.length + 1))
+        let mixed_answers = incorrect_answers.map(a => a);
+        mixed_answers.splice(ranIdx, 0, correct_answer)
+        return mixed_answers
+    }
     
     useEffect(() => {
         setContent({ 
-            question: qa.contents[0].question, 
-            // answer: qa.contents[0].incorrect_answers.splice((Math.floor(Math.random() * (qa.contents[0].incorrect_answers.length + 1))), 0, qa.contents[0].correct_answer)
+            question: qa.contents[nQsAnsed].question, 
+            answers: mixAns(qa.contents[nQsAnsed].incorrect_answers, qa.contents[nQsAnsed].correct_answer)
         })
-    },[])
+    }, [nQsAnsed])
+
+    const handleChange = (e) => {
+        if(e.target.checked) {
+            setAnswer(e.target.value)
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`${nQsAnsed}/${qa.settings.amount} : ${answer}`);
+        dispatch(store_answers({questionNo: nQsAnsed, answer: answer}))
+        qa.settings.amount === (nQsAnsed + 1)?  submitAnswers() : setNQsAnsed(prev => prev + 1);
+    }
+
+    function submitAnswers(){
+        setNQsAnsed(prev => prev = 0) // Loop back for Dev
+        // socket.emit('submit-results', username, scores)
+        // navigate('/results');
+    }
 
     
 
@@ -44,37 +79,41 @@ export default function Game() {
                 <div id="progress"></div>
             </div>
             <div id='answerBox'>
-                <form id='answerForm'>
+                <form id='answerForm' onSubmit={handleSubmit}>
 
-                    <input type='radio' name='answer' value={1} id='a1'></input>
+                    <input type='radio' name='answer' value={content.answers[0]} checked={answer === content.answers[0]} onChange={handleChange} id='a1'></input>
                     <label htmlFor="a1">
                     <div id="answer1">
-                        a. answer 1
+                        {/* a. answer 1 */}
+                        {content.answers[0]}
                     </div>
                     </label>
 
-                    <input type='radio' name='answer' value={2} id='a2'></input>
+                    <input type='radio' name='answer' value={content.answers[1]} checked={answer === content.answers[1]} onChange={handleChange} id='a2'></input>
                     <label htmlFor="a2">
                     <div id="answer2">
-                        b. answer 2
+                        {/* b. answer 2 */}
+                        {content.answers[1]}
                     </div>
                     </label>
 
-                    <input type='radio' name='answer' value={3} id='a3'></input>
+                    <input type='radio' name='answer' value={content.answers[2]} checked={answer === content.answers[2]} onChange={handleChange} id='a3'></input>
                     <label htmlFor="a3">
                     <div id="answer3">
-                        c. answer 3
+                        {/* c. answer 3 */}
+                        {content.answers[2]}
                     </div>
                     </label>
 
-                    <input type='radio' name='answer' value={4} id='a4'></input>
+                    <input type='radio' name='answer' value={content.answers[3]} checked={answer === content.answers[3]} onChange={handleChange} id='a4'></input>
                     <label htmlFor="a4">
                     <div id="answer4">
-                        d. answer 4
+                        {/* d. answer 4 */}
+                        {content.answers[3]}
                     </div>
                     </label>
 
-                    <input type='submit' value='Submit'></input>
+                    <input type='submit' value='Submit' />
 
                 </form>
                 <div id='otherPlayer' className='hide'><div>{otherPlayer} is playing...</div></div>
--- src/actions/socket/socketSlice.js
+++ src/actions/socket/socketSlice.js
@@ -4,9 +4,10 @@ export const socketSlice = createSlice({
     name: 'socket',
     initialState: {
         socket: null,
-        user: { username: "WHO", isHost: false },
+        user: { username: "WHO", isHost: false, scores:0 },
         room: { roomID: null, players: [] },
         qa: null,
+        submittedAns:[],
     },
     reducers: {
         store_socket: ( state, action ) => {
@@ -22,9 +23,15 @@ export const socketSlice = createSlice({
         store_qa: ( state, action ) => {
             state.qa = action.payload
         },
+        store_answers: ( state, action ) => {
+            state.submittedAns.push(action.payload.answer)
+            if(state.qa.contents[action.payload.questionNo].correct_answer === action.payload.answer){
+                state.user.scores += state.qa.settings.settingCoef
+            }
+        },
     },
 })
 
-export const { store_socket, store_user, update_room, store_qa } = socketSlice.actions
+export const { store_socket, store_user, update_room, store_qa, store_answers } = socketSlice.actions
 
 export default socketSlice.reducer
