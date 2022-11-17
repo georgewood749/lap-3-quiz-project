9888267a128c03109d895695c802a9d92f74efdc
aimyv <av533@bath.ac.uk>
2022-11-17 14:31:15 +0000

    edited welcome page styling

--- src/Pages/Menu/index.js
+++ src/Pages/Menu/index.js
@@ -3,26 +3,27 @@ import { Link } from 'react-router-dom';
 
 export default function Menu() {
     return (
-        <div className='main'>
+        <>
             <h1>Welcome</h1>
+            <div className='main' id='menu'>
 
-            <div className='center'>
-                <Link to='/create'>
-                    <button id='create'>Create Game</button>
-                </Link>
+                <div className='center'>
+                    <Link to='/create'>
+                        <button id='create'>Create Game</button>
+                    </Link>
 
-                <fieldset>
-                    <legend>or</legend>
-                </fieldset>
+                    <fieldset>
+                        <legend>or</legend>
+                    </fieldset>
 
-                <Link to='/join'>
-                    <button id='join'>Join Game</button>
-                </Link>
+                    <Link to='/join'>
+                        <button id='join'>Join Game</button>
+                    </Link>
+                </div>
             </div>
-
             <Link to='/leaderboard'>
                 <button id='leaderboard'>Leaderboard</button>
             </Link>
-        </div>
+        </>
     )
 }
