d64a31207f0171098b82dd6a99246f1d00fb2159
georgewood749 <georgewood749@gmail.com>
2022-11-18 10:12:09 +0000

    updated results page

--- src/Pages/Results/index.js
+++ src/Pages/Results/index.js
@@ -27,23 +27,10 @@ export default function Results() {
             <div id='winner'>
                 Congratulations first!
             </div>
+            <div className='leaderboardRow'>
             {results}
-            {/* <div className='leaderboardRow'>
-                <h2>first 🥇</h2>
-                <h2>1000</h2>
+
             </div>
-            <div className='leaderboardRow'>
-                <h2>second 🥈</h2>
-                <h2>750</h2>
-            </div>
-            <div className='leaderboardRow'>
-                <h2>third 🥉</h2>
-                <h2>500</h2>
-            </div>
-            <div className='leaderboardRow'>
-                <h2>fourth</h2>
-                <h2>250</h2>
-            </div> */}
         </div>
     )
 }
